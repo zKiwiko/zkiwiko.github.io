c87c03f9e6f758adb1515d86b47c58c907e4852c
Kiwiko <107355461+zKiwiko@users.noreply.github.com>
2024-11-26 20:22:14 -0500

    Update script.js

--- Astral/scripts/script.js
+++ Astral/scripts/script.js
@@ -1,1 +1,27 @@
+document.querySelectorAll(".tab-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    document
+      .querySelectorAll(".tab-button")
+      .forEach((btn) => btn.classList.remove("active"));
+    document
+      .querySelectorAll(".tab-content")
+      .forEach((content) => content.classList.remove("active"));
 
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
+  });
+});
+
+document.querySelectorAll(".sub-tab-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    document
+      .querySelectorAll(".sub-tab-button")
+      .forEach((btn) => btn.classList.remove("active"));
+    document
+      .querySelectorAll(".sub-tab-content")
+      .forEach((content) => content.classList.remove("active"));
+
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
+  });
+});
